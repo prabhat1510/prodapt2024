c5c4b3155b937cccdedb3dcc6f2df4a78b1d3ffc
Prabhat Chandra <prabhatc@gmail.com>
2024-03-27 13:00:04 +0530

    Added User defined exception and its usage example
    Added User defined exception and its usage example

---
+++ layeredarchitectureexample/src/layeredarchitectureexample/exceptions/UserNotFoundException.java
@@ -0,0 +1,5 @@
+package layeredarchitectureexample.exceptions;
+
+public class UserNotFoundException extends Exception{
+
+}
--- layeredarchitectureexample/src/layeredarchitectureexample/service/LoginServiceImpl.java
+++ layeredarchitectureexample/src/layeredarchitectureexample/service/LoginServiceImpl.java
@@ -2,6 +2,8 @@ package layeredarchitectureexample.service;
 
 import layeredarchitectureexample.dao.LoginDAO;
 import layeredarchitectureexample.dao.LoginDAOImpl;
+import layeredarchitectureexample.exceptions.PasswordMismatchException;
+import layeredarchitectureexample.exceptions.UserNotFoundException;
 import layeredarchitectureexample.model.Login;
 
 public class LoginServiceImpl implements LoginService {
@@ -11,16 +13,24 @@ public class LoginServiceImpl implements LoginService {
 		LoginDAO loginDAO = new LoginDAOImpl();
 		try {
 			Login loginFromDB = loginDAO.getUserDetails(login.getUserName());
-			if(login.getPassword().equals(loginFromDB.getPassword())) {
-				return "User logged in successfully";
+			if (loginFromDB != null) {
+				if (login.getPassword().equals(loginFromDB.getPassword())) {
+					return "User logged in successfully";
+				} else {
+					//throw new PasswordMismatchException();
+					throw new PasswordMismatchException("User details mismatch");
+				}
+			} else {
+				throw new UserNotFoundException();
 			}
-		}catch(Exception e) {
-			
+		} catch (UserNotFoundException e) {
+
 			return "Username doesn't exists in our system";
+		} catch (PasswordMismatchException pme) {
+			//return "User details mismatch";
+			return pme.getMessage();
 		}
-		
-		return "User details mismatch";
-		
+
 	}
 
 }
