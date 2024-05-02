c0c6bdd5a455f923461853e41a9cea7783595b99
Prabhat Chandra <prabhatc@gmail.com>
2024-05-02 15:10:44 +0530

    Added the JSP
    Added the JSP

--- servletbasedwebapp/src/main/java/servletbasedwebapp/servlet/CustomerServlet.java
+++ servletbasedwebapp/src/main/java/servletbasedwebapp/servlet/CustomerServlet.java
@@ -1,7 +1,33 @@
 package servletbasedwebapp.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import servletbasedwebapp.entities.Customer;
+import servletbasedwebapp.service.CustomerService;
+import servletbasedwebapp.service.CustomerServiceImpl;
+
+@WebServlet("/customer")
 public class CustomerServlet extends HttpServlet{
-
+	
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		CustomerService customerService = new CustomerServiceImpl();
+		Customer customer = customerService.getCustomerById(15);
+		req.setAttribute("customer",customer);
+		RequestDispatcher dispatcher =  req.getRequestDispatcher("customer.jsp");
+}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		CustomerService customerService = new CustomerServiceImpl();
+		Customer customer = customerService.addCustomer((Customer) req.getAttribute("customer"));
+		req.setAttribute("customer",customer);
+		RequestDispatcher dispatcher =  req.getRequestDispatcher("customer.jsp");
+}
 }
---
+++ servletbasedwebapp/src/main/webapp/customer.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Customer JSP</title>
+</head>
+<body>
+	Customer JSP
+	<form>
+ 
+
+	</form>
+	
+</body>
+</html>
