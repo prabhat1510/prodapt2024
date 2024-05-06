bf112366ddc82d6320a5041808ba7e68b8f46447
Prabhat Chandra <prabhatc@gmail.com>
2024-05-06 13:10:22 +0530

    HttpSession Example
    HttpSession Example

--- sessionmgmtexample/src/main/java/sessionmgmtexample/servlet/sessionexample/LoginFormSessionServlet.java
+++ sessionmgmtexample/src/main/java/sessionmgmtexample/servlet/sessionexample/LoginFormSessionServlet.java
@@ -1,29 +1,24 @@
-/**package sessionmgmtexample.servlet.cookieexample;
+package sessionmgmtexample.servlet.sessionexample;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LoginServlet
- */
-/**@WebServlet("/")
-public class LoginFormServlet extends HttpServlet {
+@WebServlet("/LoginSessionServlet")
+public class LoginFormSessionServlet extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-			RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/loginsession.html");
 			rd.forward(request, response);
 
 
 	}
 
-}**/
+}
