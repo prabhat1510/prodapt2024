65c3f41f70136f3142a35f82ceabb71fac89d9c4
Prabhat Chandra <prabhatc@gmail.com>
2024-05-08 14:19:40 +0530

    Autowiring modes examples
    Autowiring modes examples

---
+++ autowiringbytype/src/main/java/autowiringbytype/beans/Employee.java
@@ -0,0 +1,22 @@
+package autowiringbytype.beans;
+
+public class Employee {
+	private String empName;
+	private Address address;
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+}
