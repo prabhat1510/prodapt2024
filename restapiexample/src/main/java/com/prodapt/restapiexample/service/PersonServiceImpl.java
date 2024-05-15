c8ae4cb1049b13e137c2307d9df5cc75bfa5ac63
Prabhat Chandra <prabhatc@gmail.com>
2024-05-15 13:14:01 +0530

    Added Logging feature
    Added Logging feature

--- restapiexample/src/main/java/com/prodapt/restapiexample/service/PersonServiceImpl.java
+++ restapiexample/src/main/java/com/prodapt/restapiexample/service/PersonServiceImpl.java
@@ -2,6 +2,8 @@ package com.prodapt.restapiexample.service;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,8 @@ import com.prodapt.restapiexample.repositories.PersonRepository;
 
 @Service
 public class PersonServiceImpl implements PersonService {
+	 // Get the SLF4J logger interface, default Logback, a SLF4J implementation
+    private static final Logger logger = LoggerFactory.getLogger(PersonServiceImpl.class);
 
 	@Autowired
 	private PersonRepository repo;
@@ -23,8 +27,10 @@ public class PersonServiceImpl implements PersonService {
 	public Person getPersonById(Integer personId) throws PersonNotFoundException {
 		Optional<Person> person = repo.findById(personId);
 		if(person.isPresent()) {
+			logger.info("Person with person id "+personId+" exists in record");
 			return person.get();
 		}else {
+			logger.error("Person with personId "+personId+"doesn't exists");
 			throw new PersonNotFoundException("Person with personId "+personId+"doesn't exists");
 		}
 		
