58c83fd3918a5b439f8876060d63684d7ec4ec06
Prabhat Chandra <prabhatc@gmail.com>
2024-05-22 10:07:34 +0530

    JavaScript Basic Examples
    JavaScript Basic Examples

---
+++ jsproject/src/js/hello.js
@@ -0,0 +1,10 @@
+console.log("I am external file with message Hello !!!");
+let num1 = 10;//declaring variable using let and initializing with value 10
+let num2 = 15.50;//declaring variable using let and initializing with value 15.50
+
+add(num1,num2);//calling the add function
+
+//Function add defined
+function add(num1,num2){
+    console.log(num1+num2);
+}
