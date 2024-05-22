39f79e81e39184120afde7db552ac4f0fc7d43e1
Prabhat Chandra <prabhatc@gmail.com>
2024-05-22 14:33:52 +0530

    Arrays Example
    Arrays Example

---
+++ jsproject/src/js/arraysexample.js
@@ -0,0 +1,13 @@
+/**
+ * An array is a special variable, which can hold more than one value
+ * In JS an array is an object datatype 
+ */
+const students=["Karthik","Rajesh","Mohit"]; //arrays of strings
+const numbers=[4,5,6,8,10,9,7];//arrays of numbers
+const mixvalues=["Karthik",5,5.55,new Date()];//Different datatypes data is allowed in JS arrays
+console.log(mixvalues);
+console.log(numbers);
+console.log(students);
+console.log(typeof(mixvalues));
+console.log(typeof(numbers));
+console.log(typeof(students));
