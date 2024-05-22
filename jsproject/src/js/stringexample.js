6753882a39e64c3c318f91db87ec05d963887288
Prabhat Chandra <prabhatc@gmail.com>
2024-05-22 12:38:29 +0530

    Updated string === and == equals examples
    Updated string === and == equals examples

--- jsproject/src/js/stringexample.js
+++ jsproject/src/js/stringexample.js
@@ -71,7 +71,7 @@ console.log(name == n);
 
 
 /***
- * It is know as loose equality operator because it checks abstract equality i.e.
+ * It is known as loose equality operator because it checks abstract equality i.e.
  * it tends to convert the data types of operands in order to carry the comparison
  * when two operands aren't of the same data type.
  * 
@@ -103,3 +103,57 @@ console.log(typeof(c));
 console.log(typeof(d));
 console.log(c === d);
 console.log("******************************");
+
+/**
+ * let a = 10;
+    let b = '10';
+    Strict equality :---
+   #1  If the operands that we are comparing are of different data type, then it returns false
+   #2 If any one of the two operands that we are comparing is NaN, then it will return false
+   #3 if the operands that we are comparing are null or undefined , then it will retun true.
+   #4  If the operands that we are comparing are objects, then it will return true if both refer to the same object, else it will return false.
+ */
+   let e = 10;
+   console.log(a === b); // output is false
+   console.log(a === e); // true
+   console.log(b === e); // false
+   console.log("************Strict equality #1 When both operands are of different data type******************");
+   let n1 = 34;
+   let n2 = '34';
+   console.log(n1 === n2); // false
+   console.log(n1 !== n2); //true
+   
+   
+   console.log("************Strict equality #2 When either of the operands is NaN ******************");
+   let n3 = NaN;
+   let n4 = 34;
+   console.log(n3 === n4); // false
+   console.log(n3 !== n4); //true
+   
+   
+   console.log("************Strict equality #3 When either of the operands is null or undefined ******************");
+   let n5 = null;
+   let n6 = undefined;
+   let n7 = undefined;
+   let n8 = null;
+   console.log(n5 === n8); // true
+   console.log(n5 !== n8); //false
+   console.log(n5 === n6); // false
+   console.log(n6 === n7); // true
+   
+   console.log("************Strict equality #4 When both operands are object ******************");
+   let car1 = {
+       name : "Audi"
+   };
+   
+   let car2 = {
+       name : "Audi"
+   };
+   
+   console.log(car1 === car1); //true -- its refering to the same instance or objects
+   
+   console.log(car1 === car2); //false -- its refering to the different instance
+   //!== strict inequality 
+   // != inequality loosely 
+   console.log(car1.name);
+   
